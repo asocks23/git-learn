e3d84278589f55227abb819d6de10a6156e0568a
asocks <asocks@163.com>
2021-12-05 13:36:52 +0800

    Third Commit!!!

--- src/main/java/org/zzia/info/Launch.java
+++ src/main/java/org/zzia/info/Launch.java
@@ -4,5 +4,7 @@ public class Launch {
     public static void main(String[] args) {
         System.out.println("learning git !!!");
         System.out.println("commit 222222222222222 !!!");
+        System.out.println("commit 333333333333333 !!!");
+
     }
 }
