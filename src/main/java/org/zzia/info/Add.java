51718beb5d73d4743d8533f9a6c69bd3bff60a65
asocks <asocks@163.com>
2021-12-11 17:18:50 +0800

    ===========

---
+++ src/main/java/org/zzia/info/Add.java
@@ -0,0 +1,7 @@
+package org.zzia.info;
+
+public class Add {
+    public static void main(String[] args) {
+        System.out.println("============test add file==***********============");
+    }
+}
