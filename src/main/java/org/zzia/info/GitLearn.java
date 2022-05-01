9c23ec6d2f9db62a9282cd4d2e4b6bd2f660a8aa
asocks <asocks@163.com>
2022-05-01 17:58:48 +0800

    second  commit 2222222222!!!

--- src/main/java/org/zzia/info/GitLearn.java
+++ src/main/java/org/zzia/info/GitLearn.java
@@ -3,5 +3,6 @@ package org.zzia.info;
 public class GitLearn {
     public static void main(String[] args) {
         System.out.println("first  commit !!!");
+        System.out.println("second  commit 2222222222!!!");
     }
 }
